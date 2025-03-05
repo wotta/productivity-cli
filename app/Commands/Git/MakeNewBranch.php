3a0af6aad46980733bd7205efa94f3b032499e77
Wouter <wouter.van.marrum@protonmail.com>
2025-03-05 23:12:59 +0100

    feat: Implemented logic to run jira request

--- app/Commands/Git/MakeNewBranch.php
+++ app/Commands/Git/MakeNewBranch.php
@@ -4,7 +4,10 @@ declare(strict_types=1);
 
 namespace App\Commands\Git;
 
+use App\Http\Connectors\Jira\JiraConnector;
+use App\Http\Requests\Jira\Issue\GetSearchIssuesRequest;
 use Illuminate\Console\Scheduling\Schedule;
+use Illuminate\Support\Facades\Cache;
 use LaravelZero\Framework\Commands\Command;
 
 class MakeNewBranch extends Command
@@ -28,6 +31,18 @@ class MakeNewBranch extends Command
      */
     public function handle(): int
     {
+        if (! $this->hasJiraAuthenticationSettings()) {
+            return Command::FAILURE;
+        }
+
+        $jira = new JiraConnector(
+            siteUrl: Cache::get('jira.site-url'),
+            email: Cache::get('jira.email'),
+            apiToken: Cache::get('jira.api-token'),
+        );
+
+        dd($jira->send(new GetSearchIssuesRequest())->json());
+
         return Command::SUCCESS;
     }
 
@@ -37,5 +52,20 @@ class MakeNewBranch extends Command
     public function schedule(Schedule $schedule): void
     {
         // $schedule->command(static::class)->everyMinute();
+    }
+
+    private function hasJiraAuthenticationSettings(): bool
+    {
+        $siteUrl = Cache::get('jira.site-url');
+        $email = Cache::get('jira.email');
+        $apiToken = Cache::get('jira.api-token');
+
+        if (empty($siteUrl) || empty($email) || empty($apiToken)) {
+            $this->error('No site-url, email, or api-token found');
+
+            return false;
+        }
+
+        return true;
     }
 }
