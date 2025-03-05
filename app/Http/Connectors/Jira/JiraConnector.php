6819cf0e45629f72336adef54a190069f6fb65eb
Wouter <wouter.van.marrum@protonmail.com>
2025-03-05 23:12:00 +0100

    feat: Added authentication settings for Jira connector

--- app/Http/Connectors/Jira/JiraConnector.php
+++ app/Http/Connectors/Jira/JiraConnector.php
@@ -4,13 +4,29 @@ declare(strict_types=1);
 
 namespace App\Http\Connectors\Jira;
 
+use Saloon\Contracts\Authenticator;
+use Saloon\Http\Auth\BasicAuthenticator;
 use Saloon\Http\Connector;
 
 class JiraConnector extends Connector
 {
+    public function __construct(
+        public readonly string $siteUrl,
+        public readonly string $email,
+        public readonly string $apiToken,
+    ) {}
+
     public function resolveBaseUrl(): string
     {
-        return sprintf('https://%s.atlassian.net/rest/api/3/', config('services.jira.domain'));
+        return sprintf('https://%s.atlassian.net/rest/api/3/', $this->siteUrl);
+    }
+
+    protected function defaultAuth(): ?Authenticator
+    {
+        return new BasicAuthenticator(
+            $this->email,
+            $this->apiToken,
+        );
     }
 
     protected function defaultHeaders(): array
