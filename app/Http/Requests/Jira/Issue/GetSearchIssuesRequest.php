f70daca0f21da52153adfb0eeadccada8795a17e
Wouter van Marrum <wouter.van.marrum@kaartje2go.nl>
2025-03-11 08:39:28 +0100

    feat: Updated jql to fetch projects

--- app/Http/Requests/Jira/Issue/GetSearchIssuesRequest.php
+++ app/Http/Requests/Jira/Issue/GetSearchIssuesRequest.php
@@ -14,12 +14,25 @@ class GetSearchIssuesRequest extends Request
         return '/search/jql';
     }
 
+    public function setProject(string $projectSlug): GetSearchIssuesRequest
+    {
+        $this
+            ->query()
+            ->add(
+                key: 'jql',
+                value: sprintf($this->query()->get('jql'), $projectSlug)
+            );
+
+        return $this;
+    }
+
     protected function defaultQuery(): array
     {
         return [
-            'jql' => 'project = PROJ AND status NOT IN (Done, "Closed for now") AND sprint != empty ORDER BY created DESC',
+            'jql' => 'project = %s AND status NOT IN (Done, "Closed for now") AND status IN ("To Do", "On hold", "In Progress") AND sprint in openSprints() ORDER BY created DESC',
+            'expand' => 'renderedFields',
             'maxResults' => 10,
-            'fields' => 'id,title,summary',
+            'fields' => 'id,title,summary,description',
         ];
     }
 }
